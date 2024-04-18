479986bf51e5f5fc38cbd1fd54c51f04f0a41663
manuelbrauncoder <158208453+manuelbrauncoder@users.noreply.github.com>
2024-04-18 11:46:10 +0200

    clean more code

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -57,4 +57,16 @@ class MovableObject {
     jump() {
         this.speedY = 25;
     }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = '1';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -21,7 +21,7 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);    // canvas wird gelöscht
         this.ctx.translate(this.camera_x, 0);
-        
+
         this.addObjectsToMap(this.level.backgroundObjects);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
@@ -30,22 +30,20 @@ class World {
         this.ctx.translate(-this.camera_x, 0);
 
         let self = this;                        // this funktioniert nicht in der folgenden Funktion, deswegen wird es einer Variable zugewiesen
-        requestAnimationFrame(function() {
+        requestAnimationFrame(function () {
             self.draw();
         });
     }
 
     addToMap(movableObject) {
-        if(movableObject.otherDirection) {
-            this.ctx.save();    // speichert die aktuellen ctx (context) informationen ab
-            this.ctx.translate(movableObject.width, 0); // verschiebt das bild
-            this.ctx.scale(-1, 1);  // spiegelt das bild
-            movableObject.x = movableObject.x * -1; // da auch das koordinatensystem sich dreht, muss die x Koordinate gespiegelt werden
+        if (movableObject.otherDirection) {
+            this.flipImage(movableObject);
         }
-        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
-        if(movableObject.otherDirection) {
-            movableObject.x = movableObject.x * -1;
-            this.ctx.restore();     // stellt die informationen zum ctx von oben wieder her
+        movableObject.draw(this.ctx);
+        movableObject.drawFrame(this.ctx);
+
+        if (movableObject.otherDirection) {
+            this.flipImageBack(movableObject);
         }
     }
 
@@ -53,5 +51,17 @@ class World {
         objects.forEach(o => {
             this.addToMap(o);
         });
+    }
+
+    flipImage(movableObject) {
+        this.ctx.save();    // speichert die aktuellen ctx (context) informationen ab
+        this.ctx.translate(movableObject.width, 0); // verschiebt das bild
+        this.ctx.scale(-1, 1);  // spiegelt das bild
+        movableObject.x = movableObject.x * -1; // da auch das koordinatensystem sich dreht, muss die x Koordinate gespiegelt werden
+    }
+
+    flipImageBack(movableObject) {
+        movableObject.x = movableObject.x * -1;
+        this.ctx.restore();     // stellt die informationen zum ctx von oben wieder her
     }
 }
